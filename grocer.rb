7588d46b6c62028bf6356b04ad150823955a5783
Helen Marshall <marshallhelenm@gmail.com>
2019-07-05 18:15:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,8 +37,8 @@ end
 
 def apply_clearance(cart)
   cart.keys.each do |item|
-    if cart[item[:clearance]]
-      cart[item[:price]] = (cart[item[:price]] * 0.80).round(2)
+    if cartitem[:clearance]
+      cartitem[:price] = (cart[item[:price]] * 0.80).round(2)
     end
   end
   cart
