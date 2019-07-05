adb145f00808c4fbabfeb8d54abc95bbd6c84efc
Helen Marshall <marshallhelenm@gmail.com>
2019-07-05 17:20:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,27 @@
 def consolidate_cart(cart)
-  # code here
+  cons_cart = {}
+  cart.each do |item|
+    if cons_cart[item.keys[0]]
+      cons_cart[item.keys[0]][:count] += 1
+    else
+      cons_cart[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
+    end   
+  end
+  cons_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coup_cart = []
+  cart.each do |Coupon|
+    if cart.keys.include? coupon[:item]
+      
+    else
+    end
+  end
 end
 
 def apply_clearance(cart)
