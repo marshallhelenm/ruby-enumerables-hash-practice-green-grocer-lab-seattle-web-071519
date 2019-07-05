2c57d67ba64fd200cb276fbeebc6d22209080440
Helen Marshall <marshallhelenm@gmail.com>
2019-07-05 17:48:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ def apply_coupons(cart, coupons)
         name_w_coup = "#{coupon[:item]} W/COUPON"
         if cart[name_w_coup]
           cart[name_w_coup][:count] += coupon[:num]
-        else cart[name_w_coup]
+        else 
           cart[name_w_coup] = {
             price: coupon[:cost]/coupon[:num],
             clearance: cart[coupon[:item]][:clearance],
